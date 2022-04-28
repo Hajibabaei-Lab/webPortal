086ae831d460fc464644221318455077243e56e3
Artin Mashayekhi <artinmashayekhi@protonmail.com>
2022-04-28 08:55:29 -0400

    fixed errors

--- cloropleth.R
+++ cloropleth.R
@@ -20,91 +20,67 @@ wwf_wgs84 <- wwf_read %>%
   st_buffer(0) %>% # Make invalid geometries valid
   st_transform(crs = 4326) # Convert coordinates to WGS84
 
-# simplify so that map loads faster
+# Simplify so that map loads faster
 simplified <- rmapshaper::ms_simplify(wwf_wgs84)
 
 
 ###############################
-# read in taxonomy file
-tax <- read.csv("taxonomy.csv", header=TRUE, stringsAsFactors = FALSE)
+# Read in taxonomy file
+tax <- read.csv("taxonomy.csv", header = TRUE, stringsAsFactors = FALSE)
 
-# rename Sample_Name to File_Name
+# Rename Sample_Name to File_Name
 tax$File_Name <- tax$Sample_Name
 
-# replace any dashes with underscores for separation
+# Replace any dashes with underscores for separation
 tax$File_Name <- gsub("-", "_", tax$File_Name)
 
-# split File_Number into different columns using the underscore as the separator
-tax <- cbind(tax, data.frame(do.call('rbind', strsplit(as.character(tax$File_Name),'_',fixed=TRUE))))
+# Split File_Number into different columns using the underscore as the separator
+tax <- cbind(tax, data.frame(do.call('rbind', strsplit(as.character(tax$File_Name), '_', fixed = TRUE))))
 names(tax)[34:43] <- c("Project", "Group", "Substrate", "Site", "Sample", "Replicate", "Date", "Marker", "IlluminaRun", "Amplicon")
 
-# put the field back together to match File_name in s
+# Put the field back together to match File_name in s
 tax$File_Name <- paste(tax$Project, tax$Group, tax$Substrate, tax$Site, tax$Sample, tax$Replicate, tax$Date, tax$Marker, sep="-")
 
-# create taxon field for assignments
-## for 200bp COI query, 95% confidence at species rank (sBP >= 0.70), 
+# Create taxon field for assignments
+## For 200bp COI query, 95% confidence at species rank (sBP >= 0.70), 
 ## 99% confidence genus (gBP >= 0.30), family (fBP >= 0.20), no cutoff needed for order+
 tax$taxon <- ifelse(tax$sBP >= 0.70, paste(tax$Phylum, tax$Class, tax$Species, sep=";"),
                     ifelse(tax$gBP >= 0.30, paste(tax$Phylum, tax$Class, tax$Genus, sep=";"),
-                          ifelse(tax$fBP >= 0.20, paste(tax$Phylum, tax$Class, tax$Family, sep=";"),
-                                 paste(tax$Phylum, tax$Class, tax$Order, sep=";"))))
+                           ifelse(tax$fBP >= 0.20, paste(tax$Phylum, tax$Class, tax$Family, sep=";"),
+                                  paste(tax$Phylum, tax$Class, tax$Order, sep=";"))))
 
-<<<<<<< HEAD
-# give taxon and site per File_Name
+# Give taxon and site per File_Name
 tax.stats <- tax[,c("File_Name", "taxon", "Site")]
-=======
-# give richness per File_Name
-tax.richness <- tax[,c("File_Name", "taxon")]
->>>>>>> cead98b4fa278ceac6b509342659deb559203cf7
 
 
 ###############################
-# read in metadata
-# need to map Site_Sample to WSCSDA field from simplified .shp file (see below)
+# Read in metadata
 s <- read.csv("metadata.csv", header=TRUE, stringsAsFactors = FALSE)
 
-# get unique latitude, longitude, file name, and WSCSDA
+# Get unique latitude, longitude, file name, and WSCSDA
 s2 <- unique(s[,c("File_Name", "WSCSDA", "Lat","Long")])
 
-# drop row if NA
+# Drop row(s) if NA
 s3 <- na.omit(s2)
 
-# map taxon, samples, and sites to s3 by File_Name
+# Map taxon, samples, and sites to s3 by File_Name
 tax.meta <- merge(tax.stats, s3, by = "File_Name", all.x = TRUE)
 
-<<<<<<< HEAD
-#fix this when the dataset entry is completed
+# Fix this when the dataset entry is completed
 tax.meta <- tax.meta[!tax.meta$File_Name == "STREAM-DFONLX-B-LALD-000X-X-20201011-COI",]
-=======
-# STREAM-DFONLX-B-LALD-000X-X-20201011-COI in metadata is missing info, but need it because we have sequences for it
-# Artin to get this info from Mike, until then, drop
-tax.meta <- tax.meta[!tax.meta$File_Name=="STREAM-DFONLX-B-LALD-000X-X-20201011-COI",]
 
-# now group by WSCSDA and count unique taxa
-tax.richness <- data.frame(tax.meta %>% group_by(WSCSDA) %>% dplyr::summarise(richness = n_distinct(taxon)))
-
-# add richness to the .shp file
-shp <- merge(simplified, tax.richness, by="WSCSDA", all.x=TRUE)
-# set NA to zero
-shp$richness[is.na(shp$richness)] <- 0
->>>>>>> cead98b4fa278ceac6b509342659deb559203cf7
-
-# group by WSCSDA and unique taxon counts, sites, and File_Name samples
+# Group by WSCSDA and unique taxon counts, sites, and File_Name samples
 tax.richness <- data.frame(tax.meta %>% group_by(WSCSDA) %>% summarise(richness = n_distinct(taxon)))
 tax.sites <- merge(data.frame(tax.meta %>% group_by(WSCSDA) %>% summarise(sites = n_distinct(Site))), tax.richness, all.x = TRUE)
 tax.stats <- merge(data.frame(tax.meta %>% group_by(WSCSDA) %>% summarise(samples = n_distinct(File_Name))), tax.sites, all.x = TRUE)
 
-# add richness, samples, and sites to the .shp file
+# Add richness, samples, and sites to the .shp file
 shp <- merge(simplified, tax.stats, by = "WSCSDA", all.x = TRUE)
-# set NAs to zero
+# Set NAs to zero
 shp[, 6:8][is.na(shp[, 6:8])] <- 0
 
-# create color palette
+# Create color palette
 pal.tmp <- RColorBrewer::brewer.pal(3, "BuGn")
-<<<<<<< HEAD
-=======
-
->>>>>>> cead98b4fa278ceac6b509342659deb559203cf7
 pal <- colorNumeric(palette = pal.tmp, domain = shp$richness)
 
 # Basic choropleth with leaflet
@@ -117,23 +93,14 @@ m <- leaflet(simplified) %>%
                             "Richness: ", shp$richness, "taxa<br>", 
                             "Sites: ", shp$sites, "<br>", 
                             "Samples: ", shp$samples, "<br>"))
-
-<<<<<<< HEAD
-=======
+# Display chloropleth
 m
 
 
-# adapt this script to also plot number of "Sites: and Samples:" 
-# per watershed in the popup box
-# Sites, number of unique sites
-# Samples, equivalent to number of unique filenames
->>>>>>> cead98b4fa278ceac6b509342659deb559203cf7
-
+#######################
 # adapt to summarize major taxa such as Ephemeroptera, Plecoptera, Trichoptera, Odonata, Chironomidae, Other
 # adapt to summarize number of taxa in each phylum
 # ranks for these taxa can be found at ncbi taxonomy page, ex. https://www.ncbi.nlm.nih.gov/taxonomy/?term=Ephemroptera
 # leaflet can be used to display this info as piecharts
 # put together a preliminary report in R markdown to get feedback from Mehrdad
 # https://rmarkdown.rstudio.com/
-
-
